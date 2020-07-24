76d9e7954c9780068f1dff1e93cd43907660bb8a
kupriyanov-al <68012366+kupriyanov-al@users.noreply.github.com>
2020-07-24 15:53:06 +0300

    master

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>ggggg</h1>')
+    return render(request, 'blog/index.html')
